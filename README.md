78d54b4705ee623ae30b4b513814de70d20825aa
David Dionis Soler <daviddionissoler@gmail.com>
2022-07-14 14:52:08 +0200

    Structure Database created

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# MySQL Recursive Example
+
+Prolem: We are creating a eLearning platform that has courses. Inside this courses we have modules. Inside this modules we can have lessons or (sub)modules. Inside the (sub)modules we can have lessons or (sub)modules... and so on.
+
+## Requirements
+- MySQL or MariaDB
---
+++ structure.sql
@@ -0,0 +1,26 @@
+
+CREATE DATABASE mysql_recursive;
+
+USE mysql_recursive;
+
+CREATE TABLE courses(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE modules(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    parent_is_course BOOLEAN NOT NULL DEFAULT TRUE,
+    parent_course_id INT,
+    parent_module_id INT,
+    FOREIGN KEY (parent_course_id) REFERENCES courses(id),
+    FOREIGN KEY (parent_module_id) REFERENCES modules(id)
+);
+
+CREATE TABLE lessons(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    parent_module_id INT,
+    FOREIGN KEY (parent_module_id) REFERENCES modules(id)
+);
